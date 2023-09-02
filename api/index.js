5c3f2c25ab56a59504376b1ad17accb9d368a0a3
khaled <khaledb.yahya@gmail.com>
2023-09-02 16:41:03 +0100

    conneted to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
-import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
