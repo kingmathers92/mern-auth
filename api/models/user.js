4397c88c88927e56b98b75b7c547893146497486
khaled <khaledb.yahya@gmail.com>
2023-09-04 19:50:18 +0100

    profile ui done

--- api/models/user.js
+++ api/models/user.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    profilPicture: {
+    profilePicture: {
       type: String,
       default:
         "https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg",
